700e1df3b6c7e3708479d7ae9b1d197942fe51f4
Oljas Medetbaev <medetbaevoljas@gmail.com>
2026-07-27 20:35:59 +0200

    Initial commit

---
+++ src/redux/actions/noteAction.js
@@ -0,0 +1,13 @@
+export const addNoteAction = (note) => {
+  return {
+    type: "ADD_NOTE",
+    payload: note,
+  };
+};
+
+export const deleteNoteAction = (id) => {
+  return {
+    type: "DELETE_NOTE",
+    payload: id,
+  };
+};
---
+++ src/redux/reducers/notesReducer.js
@@ -0,0 +1,28 @@
+const initialState = {
+  notes: [],
+};
+
+const notesReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case "ADD_NOTE": {
+      console.log(action.payload);
+
+      return {
+        ...state,
+        notes: [...state.notes, action.payload],
+      };
+    }
+
+    case "DELETE_NOTE": {
+      return {
+        ...state,
+        notes: state.notes.filter((note) => note.id !== action.payload),
+      };
+    }
+
+    default:
+      return state;
+  }
+};
+
+export default notesReducer;
